b43f8f523e6836ba40aa223cdfcdd8dfeb4b675a
Cristian <cefunck@miuandes.cl>
2019-04-11 15:53:36 -0400

    add controllers with actions in blank

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  get 'tickets/create'
+  get 'tickets/update'
+  get 'tickets/destroy'
+  get 'event_venues/create'
+  get 'event_venues/update'
+  get 'event_venues/destroy'
+  get 'events/create'
+  get 'events/update'
+  get 'events/destroy'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :destroy, :update]
-  resources :events, only: [:create, :destroy, :update]
-  resources :event_venues, only: [:create, :destroy, :update]
-  resources :tickets, only: [:create, :destroy, :update]
+  #resources :users, only: [:create, :destroy, :update]
+  #resources :events, only: [:create, :destroy, :update]
+  #resources :event_venues, only: [:create, :destroy, :update]
+  #resources :tickets, only: [:create, :destroy, :update]
 end
